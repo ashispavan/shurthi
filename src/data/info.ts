36b9c3ee5a6d27d108a2ab2db68212ec8ae398ec
Ashis Pavan <ashispavan@live.com>
2024-12-11 13:38:08 -0500

    Revamp

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,80 +2,91 @@ export const info = {
   baseUrl: "https://shurthi.vercel.app",
   name: "Shurthi",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Kerala, India.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 4 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `I am a passionate software engineer based in Toronto, Canada, with a strong focus on web development. With over five years of experience in the industry, I specialize in creating dynamic and responsive web applications. My expertise spans both front-end and back-end technologies, including Angular, Node.js, Java, Spring Boot and RESTful API development.
   `,
 
   experience: [
     {
-      name: "IBM",
+      name: "Application Developer - IBM",
       location: "Coimbatore, Tamil Nadu, India",
-      startDate: "Apr 2022",
-      endDate: "Present",
+      startDate: "Jun 2022",
+      endDate: "Sep 2024",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "Maintaining and updating web application for one of the major US Healthcare Insurance Providers. This application maintains records and helps generate reports.",
+        "• Developed web templates using Angular 7, Angular 9, Bootstrap, CSS and HTML in frontend and RESTful backend APIs using Spring and Hibernate to persist data in DB2 database.",
+        "• Responsible to resolve production defects and mentoring junior developers.",
+        "• Involved in application vulnerabilities remediation identified by security check tools.",
+        "• Worked on Angular version upgrades.",
+        "• Coordinated with SMEs, mainframe team and DevOps team based on the requirements.",
+        "• Worked on shell scripting in Unix server for SFTP of files."
       ],
     },
 
     {
-      name: "Infosys Ltd",
-      location: "Chennai, Tamil Nadu, India",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "Technology Analyst - Infosys",
+      location: "Bangalore, India",
+      startDate: "Jan 2022",
+      endDate: "Jun 2022",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "Build and maintained Real Time Dashboards and visualizations for Business stakeholders to monitor logs of MuleSoft APIs created as part of the project where log analytics pipeline is created using Elasticsearch, Logstash and Kibana for one of the biggest biotech companies.",
+        "• Monitor logs on servers and generate dashboards in real-time.",
+        "• Written automated queries in Kibana for proactively monitoring performance in different environments (like Dev, Stage and Production).",
+        "• Maintain change control and testing processes for all modifications and deployments.",
+        "• Participated in problem resolving, change, release and event management for Kibana."
       ],
     },
 
     {
-      name: "Interns India",
-      location: "India",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Senior System Engineer - Infosys",
+      location: "Bangalore, India",
+      startDate: "Jan 2021",
+      endDate: "Jan 2022",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "Developed web templates for Sales Opportunity Management Portal using Angular 11, Angular Material, CSS3 and HTML. Also created Backend REST APIs using NodeJS and express to persist data in Postgres database.",
+        "• Worked on integration with backend, data migration and error handling.",
+        "• Involved in bug fixes, peer code reviews and mentoring junior developers.",
+        "• Collaborated with UX designers to understand the layouts and wireframes of the UI modules.",
+        "• Developed UI modules for Enterprise Healthcare Application using Angular 11, Bootstrap, HTML and SCSS.",
+        "• Co-developed RESTful backend APIs using Spring Boot to persist data in Postgres DB.",
+        "• Managed deployment, peer code reviews and organized client demos.",
+        "• Worked closely with Architect to improve performance."
       ],
     },
+    {
+      name: "System Engineer",
+      location: "Chennai, India",
+      startDate: "Jan 2019",
+      endDate: "Jan 2021",
+      description: [
+      "Worked on Middleware development setup which involves B2B critical Business Transactions such as POs, ASN, Invoices and Shipments for various trading partners using IBM Sterling Integrator."
+      ],
+    }
   ],
 
   education: [
     {
-      name: "RIT",
+      name: "Bachelor of Engineering, Computer Science & Engineering",
       location: "Coimbatore, Tamil Nadu, India",
-      startDate: "2019",
-      endDate: "2022",
+      startDate: "2014",
+      endDate: "2018",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "Sri Ramakrishna Institute of Technology",
       ],
     },
     {
-      name: "Govt. Degree College Buffer Zone",
-      location: "India",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
+      name: "Higher Secondary Education",
+      location: "Nilgiris, Tamil Nadu, India",
+      startDate: "2012",
+      endDate: "2014",
+      description: ["Holy Cross Convent Matriculation Higher Secondary School"],
     },
     
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
     twitter: "https://twitter.com/Uzzii_21",
     github: "https://github.com/uzzii-21",
-    email: "shurthia066.com",
+    email: "shurthiappukuttan.com",
     linkedin: "https://www.linkedin.com/in/shurthi/",
   },
 
