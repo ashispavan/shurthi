e7a57b11dc51865982a766bdd5ab12c20fd39e3a
Ashis Pavan <ashispavan@live.com>
2025-02-05 12:09:25 -0500

    Updated resume

--- src/data/info.ts
+++ src/data/info.ts
@@ -12,13 +12,21 @@ export const info = {
       startDate: "Jun 2022",
       endDate: "Sep 2024",
       description: [
-        "Development and support of web application for one of the major US Healthcare Insurance Providers. This application maintains records and helps generate reports.",
-        "• Developed web templates using Angular 7, Angular 9, Bootstrap, CSS and HTML in frontend and RESTful backend APIs using Spring and Hibernate to persist data in DB2 database.",
-        "• Responsible to resolve production defects and mentoring junior developers.",
-        "• Involved in application vulnerabilities remediation identified by security check tools.",
-        "• Worked on Angular version upgrades.",
-        "• Coordinated with SMEs, mainframe team and DevOps team based on the requirements.",
-        "• Worked on shell scripting in Unix server."
+        "Developed web application to maintain records and generate reports for two major US Healthcare Insurance Providers.",
+        "Built API endpoints adhering to RESTful principles to handle CRUD operations efficiently for core business functionalities.",
+        "Used Hibernate ORM to map Java objects to the DB2 relational database, enabling efficient data persistence and retrieval with minimal boilerplate code.",
+        "Integrated Spring Security to secure API endpoints, implementing JWT-based authentication and role-based access control.",
+        "Developed web templates using Angular 7, Angular 9, Bootstrap, CSS and HTML for the frontend.",
+        "Applied thorough debugging and troubleshooting techniques to identify and rectify software defects improving overall application performance by 20%.",
+        "Provided technical oversight for junior developers, guiding them throughout the software development lifecycle.",
+        "Remediated application vulnerabilities identified by security tools like Checkmarx.",
+        "Migrated legacy IBM web forms to Angular's reactive forms to enhance application scalability, maintainability, and user experience.",
+        "Optimized form performance by implementing lazy loading and change detection strategies.",
+        "Worked on Angular version upgrade from Angular 7 to Angular 11.",
+        "Coordinated with cross-functional teams using Agile Methodology to ensure efficient project completion.",
+        "Performed comprehensive testing using JUnit to validate API functionality and ensure robust handling of edge cases.",
+        "Worked on shell scripting in Unix server for data extraction, conversion and file transfer.",
+        "Experienced in incident resolution and production support."
       ],
     },
 
@@ -28,11 +36,13 @@ export const info = {
       startDate: "Jan 2022",
       endDate: "Jun 2022",
       description: [
-        "Developed real-time dashboards and visualizations for business stakeholders to monitor logs of MuleSoft APIs created as part of the project where log analytics pipeline is created using Elasticsearch, Logstash and Kibana for one of the biggest biotech companies.",
-        "• Monitor logs on servers and generate dashboards in real-time.",
-        "• Written automated queries in Kibana for proactively monitoring performance in different environments (dev, stage and production).",
-        "• Maintain change control and testing processes for all modifications and deployments.",
-        "• Participated in problem resolving, change, release and event management for Kibana."
+        "Developed real-time dashboards and visualizations for business stakeholders to monitor logs of MuleSoft APIs created as part of the project using Elasticsearch, Logstash and Kibana for one of the biggest pharmaceutical companies.",
+        "Documented the implemented features to facilitate knowledge transfer and onboarding for team members.",
+        "Monitored logs on servers and generated dashboards in real-time.",
+        "Wrote automated queries in Kibana for proactively monitoring performance in different environments like dev, stage and production.",
+        "Managed change control and testing processes to ensure smooth modifications and deployments.",
+        "Participated in problem resolving, change, release and event management for Kibana.",
+        "Successfully utilized Agile Methodology in the development and implementation of the features."
       ],
     },
 
@@ -42,14 +52,16 @@ export const info = {
       startDate: "Jan 2021",
       endDate: "Jan 2022",
       description: [
-        "Developed web templates for Sales Opportunity Management Portal using Angular 11, Angular Material, CSS3 and HTML. Also created Backend REST APIs using NodeJS and express to persist data in Postgres database.",
-        "• Worked on integration with backend, data migration and error handling.",
-        "• Involved in bug fixes, peer code reviews and mentoring junior developers.",
-        "• Collaborated with UX designers to understand the layouts and wireframes of the UI modules.",
-        "• Developed UI modules for Enterprise Healthcare Application using Angular 11, Bootstrap, HTML and SCSS.",
-        "• Co-developed RESTful backend APIs using Spring Boot to persist data in Postgres DB.",
-        "• Managed deployment, peer code reviews and organized client demos.",
-        "• Worked closely with Architect to improve performance."
+        "Developed web templates for Sales Opportunity Management Portal using Angular 11, Angular Material, CSS3 and HTML5.",
+        "Created Backend REST APIs using NodeJS and Express to persist data in Postgres database.",
+        "Worked on integration with backend, data migration and error handling.",
+        "Documented API specifications using Swagger, enabling easier collaboration with frontend and QA teams.",
+        "Involved in bug fixes, peer code reviews and mentoring junior developers.",
+        "Implemented best coding standards, improving overall code quality and readability.",
+        "Collaborated with UX designers to understand the layouts and sketching wireframes of the User Interface modules.",
+        "Developed UI modules for Enterprise Healthcare Application using Angular 11, Bootstrap, HTML and SCSS.",
+        "Co-developed RESTful backend APIs using Spring Boot and JPA to persist data in Postgres DB.",
+        "Managed deployment, CI/CD using Jenkins, and organized client demos."
       ],
     },
     {
@@ -58,7 +70,9 @@ export const info = {
       startDate: "Jan 2019",
       endDate: "Jan 2021",
       description: [
-      "Worked on Middleware development setup which involves B2B critical Business Transactions such as POs, ASN, Invoices and Shipments for various trading partners using IBM Sterling Integrator."
+        "Developed a CRUD application using Java and used Spring Data JPA to interact with a PostgreSQL database.",
+        "Created a Java program to read, parse, and process CSV data files efficiently.",
+        "Worked on Middleware development setup which involves B2B critical Business transactions such as POs, ASN, Invoices and Shipments for various trading partners using IBM Sterling Integrator."
       ],
     }
   ],
@@ -85,7 +99,7 @@ export const info = {
 
   socialMedia: {
     twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
+    github: "https://github.com/shurthi-dev",
     email: "shurthiappukuttan@gmail.com",
     linkedin: "https://www.linkedin.com/in/shurthi/",
   },
